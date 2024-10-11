6d0f561f54c6f572a6d5508bdf56925b24f86c6c
ev0clu <ev0clu@gmail.com>
2024-10-11 16:42:32 +0200

    Initial project setup

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import githubImgSrc from "../../../public/github-mark.svg";
+
+const Footer = () => {
+  return (
+    <footer className="flex h-8 w-full flex-row items-center justify-center gap-2 px-5 py-4">
+      <p>Copyright © Laszlo Kis {new Date().getFullYear()}</p>
+      <a
+        className="flex items-center justify-center text-xl text-gray-950 hover:text-neutral-400"
+        href="https://github.com/ev0clu"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          src={githubImgSrc}
+          priority
+          alt="github-logo"
+          className={"h-5 w-5 rounded-full hover:opacity-60"}
+        ></Image>
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
