0361d0261b71425ffa63320181c43bdf666da265
ev0clu <ev0clu@gmail.com>
2024-10-17 16:50:26 +0200

    PlanFeature component is created

---
+++ src/components/PlanFeature.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+import { CircleCheck, CircleMinus } from "lucide-react";
+
+interface PlanFeatureProps {
+  isAccess: boolean;
+  value?: number;
+  text: string;
+}
+
+const PlanFeature = ({ isAccess, value, text }: PlanFeatureProps) => {
+  return (
+    <div className="flex flex-row gap-5">
+      {isAccess ? (
+        <CircleCheck className="text-primary" />
+      ) : (
+        <CircleMinus className="text-gray-400" />
+      )}
+      <p>
+        {value && <span className="font-extrabold">{value}</span>}{" "}
+        <span className={cn({ "text-gray-400": !isAccess })}>{text}</span>
+      </p>
+    </div>
+  );
+};
+
+export default PlanFeature;
