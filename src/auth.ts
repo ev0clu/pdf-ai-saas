fe14f61b240a26f124dd33b5e4b297e41dbf9fcb
ev0clu <ev0clu@gmail.com>
2024-10-21 17:11:28 +0200

    Prisma is added to the project. Auth handling is updated with prisma adaptor.

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,23 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "../prisma/prisma";
 
 const authOptions: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
+  session: { strategy: "database", maxAge: 24 * 60 * 60 }, // 1 day -> 24h * 60min * 60sec
+  callbacks: {
+    /*authorized({ request, auth }) {
+      const { pathname } = request.nextUrl;
+      if (pathname === "/dashboard") return !!auth;
+      return true;
+    },*/
+    async session({ session, user }) {
+      session.user = user;
+      return session;
+    },
+  },
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
