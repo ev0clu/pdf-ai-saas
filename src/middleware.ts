02b7d911f492f1a89d3158604bd863ade548b090
ev0clu <ev0clu@gmail.com>
2024-10-21 17:12:04 +0200

    Middleware is updated to handle dashboard route in case of non authorized user.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware } from "@/auth";
+//export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname === "/dashboard") {
+    const newUrl = new URL("/", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+/*
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|pricing).*)"],
+};*/
