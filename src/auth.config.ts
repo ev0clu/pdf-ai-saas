f9f48ef3f964e6525afd51bbb8d94843ab34697d
ev0clu <ev0clu@gmail.com>
2024-11-07 15:54:10 +0100

    Auth config is updated to handle user plan

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,26 +1,35 @@
+import "next-auth/jwt";
 import Google from "next-auth/providers/google";
-import type { NextAuthConfig } from "next-auth";
+import type { DefaultSession, NextAuthConfig } from "next-auth";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [Google],
   basePath: "/api/auth",
   callbacks: {
-    authorized({ request, auth }) {
+    async authorized({ request, auth }) {
       const { pathname } = request.nextUrl;
       if (pathname === "/dashboard") return !!auth;
       return true;
     },
-    async jwt({ token, trigger, session, account }) {
+    async jwt({ token, trigger, session, account, user }) {
       if (trigger === "update") token.name = session.user.name;
       if (account?.provider === "google") {
-        return { ...token, accessToken: account.access_token };
+        return {
+          ...token,
+          accessToken: account.access_token,
+          user: {
+            ...token.user,
+            plan: user.plan,
+          },
+        };
       }
       return token;
     },
     async session({ session, token }) {
       if (token?.accessToken && typeof token?.accessToken === "string") {
         session.accessToken = token.accessToken;
+        session.user.plan = token.user.plan;
       }
       return session;
     },
@@ -28,16 +37,22 @@ export default {
 } satisfies NextAuthConfig;
 
 declare module "next-auth" {
+  interface User {
+    plan: "FREE" | "PRO";
+  }
   interface Session {
     accessToken?: string;
-  }
-  interface JWT {
-    accessToken?: string;
+    user: {
+      plan: "FREE" | "PRO";
+    } & DefaultSession["user"];
   }
 }
 
-declare module "@auth/core/jwt" {
+declare module "next-auth/jwt" {
   interface JWT {
     accessToken?: string;
+    user: {
+      plan: "FREE" | "PRO";
+    };
   }
 }
