ddd302cd34f5de6457425b542adaeba3e9ae3ddc
ev0clu <ev0clu@gmail.com>
2024-11-08 16:33:30 +0100

    API route has been created for GET documents request

---
+++ src/app/api/file/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../../prisma/prisma";
+import type { Document } from "@/types/document";
+import { auth } from "@/auth";
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  try {
+    const session = await auth();
+
+    const documents = (await prisma.document.findMany({
+      where: {
+        userId: session?.user.id,
+      },
+    })) as Document[] | [];
+
+    return NextResponse.json(
+      {
+        documents,
+        message: "All documents records are returned",
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error,
+      },
+      { status: 500 },
+    );
+  }
+}
