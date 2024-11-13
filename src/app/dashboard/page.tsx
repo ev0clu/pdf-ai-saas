e295f59ee5c9b3be8e308e38985aa7565a9eac46
ev0clu <ev0clu@gmail.com>
2024-11-13 16:54:52 +0100

    Dashboard component is updated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,14 @@
 import Dropzone from "./_components/Dropzone";
+import MyFiles from "./_components/MyFiles";
 
 const Dashboard = () => {
   return (
     <div>
-      <Dropzone />
+      <div className="mb-7 flex flex-row justify-between border-b-2 pb-2">
+        <h1>My files</h1>
+        <Dropzone />
+      </div>
+      <MyFiles />
     </div>
   );
 };
