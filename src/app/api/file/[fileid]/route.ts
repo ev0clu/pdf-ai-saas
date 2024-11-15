89a13a74bb0ea82046c7ba9e29013780fab22b4f
ev0clu <ev0clu@gmail.com>
2024-11-15 13:38:39 +0100

    API is created to get a unique document

--- src/app/api/file/[fileid]/route.ts
+++ src/app/api/file/[fileid]/route.ts
@@ -1,6 +1,36 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../../prisma/prisma";
 
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ fileid: string }> },
+) {
+  try {
+    const fileId = (await params).fileid;
+
+    const document = (await prisma.document.findUnique({
+      where: {
+        id: fileId,
+      },
+    })) as Document | null;
+
+    return NextResponse.json(
+      {
+        document,
+        message: "Document all records are returned",
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error,
+      },
+      { status: 500 },
+    );
+  }
+}
+
 export async function DELETE(
   req: Request,
   { params }: { params: Promise<{ fileid: string }> },
