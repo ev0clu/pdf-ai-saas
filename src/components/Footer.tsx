edd64f89e7b1b730b5bfab0e8c503045efd52661
ev0clu <ev0clu@gmail.com>
2024-11-18 08:31:45 +0100

    HTML Link element has been replaced with next link element

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import githubImgSrc from "../../public/github-mark.svg";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="mt-5 flex h-8 w-full flex-row items-center justify-center gap-2 px-5 py-6">
       <p>Copyright © Laszlo Kis {new Date().getFullYear()}</p>
-      <a
+      <Link
         className="flex items-center justify-center text-xl text-gray-950 hover:text-neutral-400"
         href="https://github.com/ev0clu"
         target="_blank"
@@ -17,7 +18,7 @@ const Footer = () => {
           alt="github-logo"
           className={"h-5 w-5 rounded-full hover:opacity-60"}
         ></Image>
-      </a>
+      </Link>
     </footer>
   );
 };
