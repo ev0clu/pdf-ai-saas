ac92dd3bcdc199658ff98ba1345a6e6d9b23cc69
ev0clu <ev0clu@gmail.com>
2024-11-18 09:32:56 +0100

    ContainerWrapper component is added to pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,16 @@
+import ContainerWrapper from "@/components/ContainerWrapper";
 import Dropzone from "./_components/Dropzone";
 import MyFiles from "./_components/MyFiles";
 
 const Dashboard = () => {
   return (
-    <div>
+    <ContainerWrapper className="mt-7 md:mt-14">
       <div className="mb-7 flex flex-row justify-between border-b-2 pb-2">
         <h1>My files</h1>
         <Dropzone />
       </div>
       <MyFiles />
-    </div>
+    </ContainerWrapper>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ContainerWrapper from "@/components/ContainerWrapper";
 import {
   StepperBarWrapper,
   StepperBar,
@@ -8,56 +9,58 @@ import {
 
 export default function Home() {
   return (
-    <div className="mx-auto">
-      <p className="max-w-4xl text-center text-4xl font-bold tracking-wide md:text-6xl lg:text-7xl">
-        Chat with your <span className="text-primary">PDF</span> documents
-      </p>
-      <p className="max-w-prose text-center text-muted-foreground md:text-lg">
-        Let&apos;s use the power of AI and get an overview about any PDF in
-        seconds.{" "}
-        <span className="decoration-slice font-bold text-black underline decoration-primary decoration-wavy">
-          QuiriPDF
-        </span>{" "}
-        is a tool which use artifact intelligent and answer to any questions
-        about the uploaded PDF document very quick.
-      </p>
+    <ContainerWrapper>
+      <div className="mx-auto">
+        <p className="max-w-4xl text-center text-4xl font-bold tracking-wide md:text-6xl lg:text-7xl">
+          Chat with your <span className="text-primary">PDF</span> documents
+        </p>
+        <p className="max-w-prose text-center text-muted-foreground md:text-lg">
+          Let&apos;s use the power of AI and get an overview about any PDF in
+          seconds.{" "}
+          <span className="decoration-slice font-bold text-black underline decoration-primary decoration-wavy">
+            QuiriPDF
+          </span>{" "}
+          is a tool which use artifact intelligent and answer to any questions
+          about the uploaded PDF document very quick.
+        </p>
 
-      <div>
-        <h1 className="mt-14 text-center md:mt-28">
-          Just a few steps need to chat
-        </h1>
         <div>
-          <div className="my-12 flex flex-row gap-10">
-            <StepperBarWrapper>
-              <StepperBar>1</StepperBar>
-              <StepperSeparator />
-              <StepperBar>2</StepperBar>
-              <StepperSeparator />
-              <StepperBar>3</StepperBar>
-              <StepperSeparator />
-              <StepperBar>4</StepperBar>
-            </StepperBarWrapper>
-            <StepperContentWrapper>
-              <StepperContent
-                title="Sign up"
-                content="Create a free account."
-              />
-              <StepperContent
-                title="Upload PDF document"
-                content="Easily upload the PDF document and make it ready for chat."
-              />
-              <StepperContent
-                title="Ask questions"
-                content="Extract informations and summarize document with AI."
-              />
-              <StepperContent
-                title="Upgrade your account"
-                content="Get more benefits with Pro Plan."
-              />
-            </StepperContentWrapper>
+          <h1 className="mt-14 text-center md:mt-28">
+            Just a few steps need to chat
+          </h1>
+          <div>
+            <div className="my-12 flex flex-row gap-10">
+              <StepperBarWrapper>
+                <StepperBar>1</StepperBar>
+                <StepperSeparator />
+                <StepperBar>2</StepperBar>
+                <StepperSeparator />
+                <StepperBar>3</StepperBar>
+                <StepperSeparator />
+                <StepperBar>4</StepperBar>
+              </StepperBarWrapper>
+              <StepperContentWrapper>
+                <StepperContent
+                  title="Sign up"
+                  content="Create a free account."
+                />
+                <StepperContent
+                  title="Upload PDF document"
+                  content="Easily upload the PDF document and make it ready for chat."
+                />
+                <StepperContent
+                  title="Ask questions"
+                  content="Extract informations and summarize document with AI."
+                />
+                <StepperContent
+                  title="Upgrade your account"
+                  content="Get more benefits with Pro Plan."
+                />
+              </StepperContentWrapper>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ContainerWrapper>
   );
 }
