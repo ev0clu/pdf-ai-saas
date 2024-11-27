1dcdf688faa37713d11285688263b8d20d9b6d58
ev0clu <ev0clu@gmail.com>
2024-11-27 12:25:11 +0100

    Functions for OpenAI chat model init  and conversation result with chat history are created

--- src/lib/vector-store.ts
+++ src/lib/vector-store.ts
@@ -1,9 +1,24 @@
 import type { Pinecone } from "@pinecone-database/pinecone";
 import { PineconeStore } from "@langchain/pinecone";
-import { OpenAIEmbeddings } from "@langchain/openai";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from "@langchain/core/prompts";
+import {
+  AIMessage,
+  HumanMessage,
+  type BaseMessage,
+} from "@langchain/core/messages";
 import { env } from "@/lib/env";
+import type { Message } from "@/types/message";
 
 let embeddingsClient: OpenAIEmbeddings | null = null;
+let chatClient: ChatOpenAI | null = null;
+const chatHistory: BaseMessage[] = [];
 
 async function initEmbeddingsClient() {
   try {
@@ -13,6 +28,20 @@ async function initEmbeddingsClient() {
     });
 
     return embeddingsClient;
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+}
+
+async function initChatClient() {
+  try {
+    const chatClient = new ChatOpenAI({
+      model: "gpt-4o",
+      temperature: 0,
+      apiKey: env.OPENAI_API_KEY,
+    });
+
+    return chatClient;
   } catch (error) {
     throw new Error(`${error}`);
   }
@@ -60,6 +89,120 @@ export async function deleteFromVectorStore(
   try {
     const vectorStore = await getVectorStore(pineconeClient);
     await vectorStore.delete({ deleteAll: true, namespace });
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+}
+
+export async function getConversationalRagChainWithHistory(
+  pineconeClient: Pinecone,
+  namespace: string,
+  text: string,
+  messages: Message[],
+) {
+  try {
+    const vectorStore = await getVectorStore(pineconeClient, namespace);
+    //const similaritySearchResult = await vectorStore.similaritySearch(query, 1);
+    const retriever = vectorStore.asRetriever();
+
+    if (!chatClient) {
+      chatClient = await initChatClient();
+    }
+
+    // Contextualize question - Chat history
+    const contextualizeQuestionSystemPrompt =
+      "Given a chat history and the latest user question " +
+      "which might reference context in the chat history, " +
+      "formulate a standalone question which can be understood " +
+      "without the chat history. Do NOT answer the question, " +
+      "just reformulate it if needed and otherwise return it as is.";
+
+    const contextualizeQuestionPrompt = ChatPromptTemplate.fromMessages([
+      ["system", contextualizeQuestionSystemPrompt],
+      new MessagesPlaceholder("chat_history"),
+      ["human", "{input}"],
+    ]);
+
+    const historyAwareRetriever = await createHistoryAwareRetriever({
+      llm: chatClient,
+      retriever: retriever,
+      rephrasePrompt: contextualizeQuestionPrompt,
+    });
+
+    // Answer question
+    const systemPrompt =
+      "You are an assistant for question-answering tasks. " +
+      "Use the following pieces of retrieved context to answer " +
+      "the question. If you don't know the answer, say that you " +
+      "don't know. Use three sentences maximum and keep the " +
+      "answer concise." +
+      "\n\n" +
+      "{context}";
+
+    const questionAnswerPrompt = ChatPromptTemplate.fromMessages([
+      ["system", systemPrompt],
+      new MessagesPlaceholder("chat_history"),
+      ["human", "{input}"],
+    ]);
+
+    const questionAnswerChain = await createStuffDocumentsChain({
+      llm: chatClient,
+      prompt: questionAnswerPrompt,
+    });
+
+    const ragChain = await createRetrievalChain({
+      retriever: historyAwareRetriever,
+      combineDocsChain: questionAnswerChain,
+    });
+
+    if (messages.length > 0) {
+      if (chatHistory.length === 0) {
+        for (const message of messages) {
+          if (message.author === "USER") {
+            chatHistory.push(new HumanMessage(message.text));
+          }
+          if (message.author === "AI") {
+            chatHistory.push(new AIMessage(message.text));
+          }
+        }
+      } else {
+        chatHistory.push(new HumanMessage(messages[messages.length - 1].text));
+      }
+    }
+
+    const results = await ragChain.invoke({
+      input: text,
+      chat_history: chatHistory,
+    });
+    chatHistory.push(new AIMessage(results.answer));
+
+    return results;
+
+    // Statefully manage chat history
+    /* function getSessionHistory(sessionId: string): BaseChatMessageHistory {
+      if (!(sessionId in store)) {
+        store[sessionId] = new ChatMessageHistory();
+      }
+      return store[sessionId];
+    }
+
+    const conversationalRagChain = new RunnableWithMessageHistory({
+      runnable: ragChain,
+      getMessageHistory: getSessionHistory,
+      inputMessagesKey: "input",
+      historyMessagesKey: "chat_history",
+      outputMessagesKey: "answer",
+    });
+    console.log(store);
+
+    for await (const s of await conversationalRagChain.stream(
+      { input: text },
+      { configurable: { sessionId: "unique_session_id" } },
+    )) {
+      // console.log(s);
+      // console.log("----");
+    }
+    return conversationalRagChain;*/
   } catch (error) {
     throw new Error(`${error}`);
   }
