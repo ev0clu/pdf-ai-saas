2a34a44727c4c482d0380ac4744e24797aea3ce9
ev0clu <ev0clu@gmail.com>
2024-11-27 12:21:10 +0100

    Chat input component is created

---
+++ src/app/dashboard/[fileid]/_components/ChatInput.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { Send } from "lucide-react";
+import type { Message } from "@/types/message";
+
+interface ChatInputProps {
+  documentId: string;
+  messages: Message[] | [] | undefined;
+}
+
+const ChatInput = ({ documentId, messages }: ChatInputProps) => {
+  const queryClient = useQueryClient();
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const mutation = useMutation({
+    mutationFn: (text: string) =>
+      fetch(`/api/message`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ documentId, text, messages }),
+      }),
+    onSuccess: async () => {
+      setInputValue("");
+      // Invalidate and refetch
+      await queryClient.invalidateQueries({ queryKey: ["messages"] });
+    },
+  });
+
+  function handleOnChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(event.currentTarget.value);
+  }
+
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === "Enter") {
+      mutation.mutate(inputValue);
+      setInputValue(event.currentTarget.value);
+    }
+  }
+
+  return (
+    <div className="relative my-3 flex h-11 flex-row items-center gap-3 rounded-md border-2 border-stone-200">
+      <input
+        className="h-10 w-full rounded-md px-2 py-1"
+        placeholder="Ask something..."
+        onChange={handleOnChange}
+        onKeyDown={handleKeyDown}
+        value={inputValue}
+        disabled={mutation.isPending}
+      />
+      <Button
+        variant={"ghost"}
+        size={"icon"}
+        className="absolute right-0"
+        disabled={!inputValue || mutation.isPending}
+        onClick={() => mutation.mutate(inputValue)}
+      >
+        <Send className="h-5 w-5 text-primary" />
+      </Button>
+    </div>
+  );
+};
+
+export default ChatInput;
