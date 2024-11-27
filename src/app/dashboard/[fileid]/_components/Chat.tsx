4ae0872292e90d68bd715a08778d96f41d59e301
ev0clu <ev0clu@gmail.com>
2024-11-27 12:22:05 +0100

    Style is updated, input component is added

--- src/app/dashboard/[fileid]/_components/Chat.tsx
+++ src/app/dashboard/[fileid]/_components/Chat.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import { MessageSquareMore, Send } from "lucide-react";
+import { Bot, MessageSquareMore } from "lucide-react";
 import ScrollAreaWrapper from "@/components/ScrollAreaWrapper";
-import { Button } from "@/components/ui/button";
 import { useQuery } from "@tanstack/react-query";
 import type { AllMessagesResponse, Message } from "@/types/message";
 import { Loader } from "@/components/Loader";
 import ErrorWrapper from "@/components/ErrorWrapper";
 import { cn } from "@/lib/utils";
+import ChatInput from "./ChatInput";
+import { useEffect, useRef } from "react";
 
 interface ChatProps {
   id: string;
 }
 
 const Chat = ({ id }: ChatProps) => {
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
   const {
     data: messages,
     error,
@@ -34,15 +37,23 @@ const Chat = ({ id }: ChatProps) => {
     },
   });
 
+  // Auto-scroll to the latest message
+  useEffect(() => {
+    scrollRef.current?.scrollTo({ top: 0, behavior: "smooth" });
+  }, [messages]);
+
   if (error) <ErrorWrapper text={error.message} />;
 
   return (
-    <ScrollAreaWrapper className="flex flex-1 flex-col">
+    <ScrollAreaWrapper className="flex flex-col justify-end gap-1">
       {isPending ? (
         <Loader />
       ) : (
         <>
-          <div className="h-full">
+          <div
+            className="flex flex-col justify-end space-y-2 md:h-full"
+            ref={scrollRef}
+          >
             {messages?.length === 0 ? (
               <div className="flex h-full flex-col items-center justify-center gap-3 p-3">
                 <span>
@@ -57,27 +68,34 @@ const Chat = ({ id }: ChatProps) => {
                 return (
                   <div
                     key={message.id}
-                    className={cn({
-                      "text-start": message.author === "AI",
-                      "text-right": message.author === "USER",
+                    className={cn("flex w-full", {
+                      "justify-start": message.author === "AI",
+                      "justify-end": message.author === "USER",
                     })}
                   >
-                    {message.text}
+                    <div
+                      className={cn(
+                        "flex flex-row gap-2 rounded-md border border-stone-200 px-2 py-1",
+                        {
+                          "bg-primary-foreground": message.author === "AI",
+                          "bg-stone-50": message.author === "USER",
+                        },
+                      )}
+                    >
+                      {" "}
+                      {message.author === "AI" && (
+                        <span className="h-5 w-5">
+                          <Bot className="text-primary" />
+                        </span>
+                      )}
+                      <span>{message.text}</span>
+                    </div>
                   </div>
                 );
               })
             )}
           </div>
-          <div className="relative mx-3 my-3 flex h-11 flex-row items-center gap-3 rounded-md border-2 border-stone-200">
-            <input className="h-10 w-full rounded-md px-2 py-1" />
-            <Button
-              variant={"ghost"}
-              size={"icon"}
-              className="absolute right-0"
-            >
-              <Send className="h-5 w-5 text-primary" />
-            </Button>
-          </div>
+          <ChatInput documentId={id} messages={messages} />
         </>
       )}
     </ScrollAreaWrapper>
